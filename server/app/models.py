14abb158901a47602f83bbdc51c337b9d987475b
Gavin Wilson <gmanwilson@icloud.com>
2025-08-26 21:25:49 -0400

    Separated Officer and User Models

--- server/app/models.py
+++ server/app/models.py
@@ -3,10 +3,37 @@ import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime, timezone
 
-# User model | contains athlete data
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    uuid = db.Column(UUID(as_uuid=True), unique=True,
+                     nullable=False, default=uuid.uuid4)
+    first_name = db.Column(db.String(160), nullable=False, index=True)
+    last_name = db.Column(db.String(160), nullable=False)
+    kilo_access = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(
+        db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(
+        db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "kilo_access": self.kilo_access,
+            "is_admin": self.is_admin
+        }
+
+    def __repr__(self):
+        return f"<User {self.name}>"
+
+# Officer model | contains athlete data
 
 
-class User(db.Model):
+class Officer(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True)
     uuid = db.Column(UUID(as_uuid=True), unique=True,
                      nullable=False, default=uuid.uuid4)
@@ -47,7 +74,7 @@ class User(db.Model):
         return bcrypt.check_password_hash(self._password_hash, password)
 
     def __repr__(self):
-        return f"<User {self.name}>"
+        return f"<Officer {self.name}>"
 
 
 class TokenBlocklist(db.Model):
