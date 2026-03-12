4e05b4a954f63f653fabce340da76e35181b6fa1
Gavin Wilson <gmanwilson@icloud.com>
2026-03-12 16:45:31 -0400

    Add initial database tables and migration scripts

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,9 +1,11 @@
-from app import create_app, db
+from flask_migrate import Migrate
+from server.app import create_app, db
 import os
 
 env = os.getenv("ENVIRONMENT", "development")
 
 app = create_app(env)
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000,
